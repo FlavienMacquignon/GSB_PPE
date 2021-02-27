c3f3837b3a4a1120d878b18a52a9111eaee9f075
FlavienMacquignon <flavien.macquignon@fastmail.fr>
2021-02-27 14:59:57 +0100

    Auto-indent TODO supprimés

--- controleurs/c_gererFrais.php
+++ controleurs/c_gererFrais.php
@@ -21,47 +21,45 @@ $numAnnee = substr($mois, 0, 4);
 $numMois = substr($mois, 4, 2);
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 switch ($action) {
-case 'saisirFrais':
-    if ($pdo->estPremierFraisMois($idUser, $mois)) {
-        $pdo->creeNouvellesLignesFrais($idUser, $mois);
-    }
-    break;
+    case 'saisirFrais':
+        if ($pdo->estPremierFraisMois($idUser, $mois)) {
+            $pdo->creeNouvellesLignesFrais($idUser, $mois);
+        }
+        break;
 
-case 'validerMajFraisForfait':
-    $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
-    if (lesQteFraisValides($lesFrais)) {
-        $pdo->majFraisForfait($idUser, $mois, $lesFrais);
-    } else {
-        ajouterErreur('Les valeurs des frais doivent être numériques');
-        include 'vues/v_erreurs.php';
-    }
-    break;
-case 'validerCreationFrais':
-    $dateFrais = filter_input(INPUT_POST, 'dateFrais', FILTER_SANITIZE_STRING);
-    $libelle = filter_input(INPUT_POST, 'libelle', FILTER_SANITIZE_STRING);
-    $montant = filter_input(INPUT_POST, 'montant', FILTER_VALIDATE_FLOAT);
-    valideInfosFrais($dateFrais, $libelle, $montant);
-    if (nbErreurs() != 0) {
-        include 'vues/v_erreurs.php';
-    } else {
-        $pdo->creeNouveauFraisHorsForfait(
-            $idUser,
-            $mois,
-            $libelle,
-            $dateFrais,
-            $montant
-        );
-    }
-    break;
-case 'supprimerFrais':
-    $idFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_STRING);
-    $pdo->supprimerFraisHorsForfait($idFrais);
-    break;
+    case 'validerMajFraisForfait':
+        $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
+        if (lesQteFraisValides($lesFrais)) {
+            $pdo->majFraisForfait($idUser, $mois, $lesFrais);
+        } else {
+            ajouterErreur('Les valeurs des frais doivent être numériques');
+            include 'vues/v_erreurs.php';
+        }
+        break;
+    case 'validerCreationFrais':
+        $dateFrais = filter_input(INPUT_POST, 'dateFrais', FILTER_SANITIZE_STRING);
+        $libelle = filter_input(INPUT_POST, 'libelle', FILTER_SANITIZE_STRING);
+        $montant = filter_input(INPUT_POST, 'montant', FILTER_VALIDATE_FLOAT);
+        valideInfosFrais($dateFrais, $libelle, $montant);
+        if (nbErreurs() != 0) {
+            include 'vues/v_erreurs.php';
+        } else {
+            $pdo->creeNouveauFraisHorsForfait(
+                $idUser,
+                $mois,
+                $libelle,
+                $dateFrais,
+                $montant
+            );
+        }
+        break;
+    case 'supprimerFrais':
+        $idFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_STRING);
+        $pdo->supprimerFraisHorsForfait($idFrais);
+        break;
 
-case 'validerFrais':
+    case 'validerFrais':
 
-    // FIXME la page ne charge pas
-    //INFO a form type can hanle a submit button --> La variable dans c_etatFrais est passé comme ça vers le 2e cas
         $lesVisiteurs = $pdo->getTousLesVisiteurs();
         break;
 }
@@ -69,10 +67,9 @@ $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idUser, $mois);
 $lesFraisForfait = $pdo->getLesFraisForfait($idUser, $mois);
 // TODO ajouter un test ici pour servir les bonnes vues en fonction du role
 // FIXME Page saisieFrais invalide --> Problème de PDO (Nouvelles Tables)
-if($_SESSION['role']==1) {
+if ($_SESSION['role'] == 1) {
     require 'vues/v_listeFraisForfait.php';
     require 'vues/v_listeFraisHorsForfait.php';
-}
-elseif($_SESSION['role']==2){
+} elseif ($_SESSION['role'] == 2) {
     require 'vues/v_Comptables/v_validerFrais_c.php';
 }
